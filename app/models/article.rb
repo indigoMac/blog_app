3923330749c343153c224b4e6e6b666776bd0e22
indigoMac <mcox@indigolighthouse.com>
2024-04-11 19:51:16 +0100

    added show actions

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+
+    # attribute :title
+    # attribute :description
+
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
