e165fe8780ce2efd36a957a24952b8308c770f34
indigoMac <mcox@indigolighthouse.com>
2024-05-01 18:00:12 +0100

    small changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
     # attribute :title
     # attribute :description
 
-    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+    validates :title, presence: true, length: { minimum: 4, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 6, maximum: 300 }
 end
